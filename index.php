8939cbfbc9b1fb6be9efd60bd3705d27da862f25
Jon Lewis <jon.lewis1@gmail.com>
2013-08-30 12:57:14 -0400

    Latest updates

---
+++ index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html>
+<head>
+<?php
+ require($_SERVER['DOCUMENT_ROOT'] . "Synergist/inc/head.html");
+ ?></head>
+
+<body>
+
+<div class="container">
+<?php
+ require($_SERVER['DOCUMENT_ROOT'] . "Synergist/inc/mainNav.html");
+ ?>
+  
+  <article class="content">
+    <h1>Latest</h1>
+    <section>
+     <h2>Get the brand-new debut album right here!</h2>
+      <p>Synergist has released their debut self-titled record! Hear the album right here!</p>
+    </section>
+    <section>
+      <h2>Upcoming Tour Dates</h2>
+      <p>Synergist could be coming to a venue near you! Check out the tour dates below: </p>
+    </section>
+    <section>
+      <h2>New Merch!</h2>
+      <p>Along with the new album, get the latest Synergist merch in the Synergist store!</p>
+    </section>
+  <!-- end .content --></article>
+  <aside>
+    <h4>Tour Dates</h4>
+    <p>TBA</p>
+  </aside>
+<?php
+ require($_SERVER['DOCUMENT_ROOT'] . "Synergist/inc/footer.html");
+ ?>
+  <!-- end .container --></div>
+</body>
+</html>
