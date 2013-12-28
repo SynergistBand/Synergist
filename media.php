78642ebe4bffc22ce0719eb7747efb2082550a42
CutchCraig <craig.mccutch@gmail.com>
2013-12-27 20:57:46 -0500

    Modal Functionality

---
+++ media.php
@@ -0,0 +1,77 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+   "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<?php
+ require($_SERVER['DOCUMENT_ROOT'] . "/Synergist/inc/head.html");
+ ?>
+
+<script type='text/javascript' src='js/jquery.simplemodal.js'></script>
+<link type='text/css' href='css/modal.css' rel='stylesheet' media='screen' />
+<script type='text/javascript' src='js/media.js'></script>
+</head>
+
+<body>
+
+<div class="container">
+<?php
+ require($_SERVER['DOCUMENT_ROOT'] . "/Synergist/inc/nav.html");
+ ?>
+  
+  <article class="content">
+    <h1>Media</h1>
+    <div>
+      <h2>Album Artwork</h2>
+      
+      
+
+      <a href='#' class='albumArt'>Balanced</a>
+      
+        <!-- modal content -->
+    <div id="modal-content">
+
+
+      <img src="images/AlbumArt/Balanced.jpg" style="max-height:100%; max-width:100%;"></div>
+        <br>&nbsp;<br>
+    </div>
+
+
+    <div class="album">
+     <h2>The Catalyst Demos (2007)</h2>
+      <img src="covers/catalyst_cover.jpg">
+      <ol>
+        <li>Greener (In the Comfort of Sun)</li>
+        <li>the Itch</li>
+        <li>Etre</li>
+        <li>Conscious Cliche</li>
+        <li>the Hourglass</li>
+      </ol>
+    </div>
+    <div class="album">
+      <h2>Synergist (2013)</h2>
+      <img src="covers/synergist_cover.jpg">
+      <ol>
+        <li>Balanced</li>
+        <li>Villain</li>
+        <li>Etre</li>
+        <li>the Hourglass</li>
+        <li>Nightlights & Skylines</li>
+        <li>Greener (In the Comfort of Sun)</li>
+        <li>the Itch</li>
+        <li>Method & Manipulation</li>
+        <li>deNihilism</li>
+        <li>Still</li>
+        <li>A Scientist's Faith</li>
+      </ol>
+    </div>
+  <!-- end .content --></article>
+  <aside>
+    <h4>Tour Dates</h4>
+    <p>TBA</p>
+  </aside>
+<?php
+ require($_SERVER['DOCUMENT_ROOT'] . "/Synergist/inc/footer.html");
+ ?>
+  <!-- end .container --></div>
+</body>
+</html>
