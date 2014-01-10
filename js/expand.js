8a1ec3de858d66e24e0bd77b401e2ff5a8cf8826
Craig Cutch <craig.mccutch@gmail.com>
2014-01-10 14:57:08 -0500

    Slight adjustments to expanding, album inc update

--- js/expand.js
+++ js/expand.js
@@ -1,14 +1,15 @@
 $(function(){
-var slideHeight = 100;
+var slideHeight = 80;
 $(".newsPost").each(function() {
     var $this = $(this);
     var $wrap = $this.children(".wrap");
     var defHeight = $wrap.height();
     if (defHeight >= slideHeight) {
-    	$wrap.children(".gradient").css("background", "url(http://localhost/Synergist/bg/bg-gradient-wide.png) no-repeat");
+    	$wrap.children(".gradient").css("background", "url(bg/bg-gradient-wide.png) no-repeat");
         var $readMore = $this.find(".read-more");
         $wrap.css("height", slideHeight + "px");
         $readMore.append("<a href='#'>Read more...</a>");
+        $readMore.css("background", "url(bg/bg-gradient-wide_rev.png) no-repeat");
         $readMore.children("a").bind("click", function(event) {
             var curHeight = $wrap.height();
             if (curHeight == slideHeight) {
@@ -17,12 +18,14 @@ $(".newsPost").each(function() {
                 }, "normal");
                 $(this).text("Close");
                 $wrap.children(".gradient").fadeOut();
+                $readMore.css("background", "none");
             } else {
                 $wrap.animate({
                     height: slideHeight
                 }, "normal");
                 $(this).text("Read more...");
                 $wrap.children(".gradient").fadeIn();
+                $readMore.css("background", "url(bg/bg-gradient-wide_rev.png) no-repeat");
             }
             return false;
         });
