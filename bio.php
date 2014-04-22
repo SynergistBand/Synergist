a1691929364ddd32bcc9802a3791277b2fb6b78d
Craig Cutch <craig.mccutch@gmail.com>
2014-04-22 11:31:46 -0400

    Bio update to match press page

--- bio.php
+++ bio.php
@@ -15,13 +15,12 @@
   <article class="content">
     <h1>Bio</h1>
     <section>
-      Synergist is a hard rock band from Toronto, Canada. Formed by longtime friends CutcH, Jon Lewis and Christopher Lyttle, 
-      the&nbsp;band blends alternative, metal, prog and punk influences to create a unique style. Striving to find 
-      a balance between songwriting and pushing themselves musically, Synergist utilizes each member's skills and tastes for a 
-      melodic heavy&nbsp;sound. 
-      <br />&nbsp;<br />
-      2013 saw the release of the Synergist's self-titled debut album, which was written, recorded and produced independently by the band. 
-      It&nbsp;is available for download at <a href="http://synergist.bandcamp.com">synergist.bandcamp.com</a>.
+      Synergist is a hard rock band from Toronto, Canada. Formed by longtime friends CutcH, Jon Lewis, and Christopher Lyttle, the band blends alternative, metal, and punk influences to create a unique style. 
+      Striving to find a balance between songwriting and pushing themselves musically, Synergist utilizes each member's skills and style for a melodic heavy sound. They have performed live at numerous venues 
+      across Ontario including the Hard Rock Café, Lee's Palace, the Velvet Underground, Sneaky Dee's and more. 
+<br>&nbsp;<br>
+2013 saw the release of Synergist's self-titled debut album, which was written, recorded and produced independently by the band. 
+It is available as a <a href="http://synergist.bandcamp.com">FREE DOWNLOAD at synergist.bandcamp.com</a>.
       <br>&nbsp;<br />
       <hr style="margin-top:15px;" />We are seeking a new bassist, and potentially other members.<br> Get in touch via email, 
       <a href="mailto:contact@synergistband.com">contact@synergistband.com</a>, or <a href="social.php">hit us up on social media!</a><hr style="margin-top:8px" /> 
